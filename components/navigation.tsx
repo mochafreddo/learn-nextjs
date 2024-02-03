2b8739847eedece244e33e527aa98c7a13ef7751
mochafreddo <corc411@gmail.com>
2024-02-04 08:48:25 +0900

    feat(app): Implement core pages and components for movie application
    - Add new loading and error handling components for home and movie details pages.
    - Implement HomePage and MovieDetailPage with asynchronous data fetching from an external API.
    - Create and style Movie, MovieInfo, and MovieVideos components for displaying movie details and related videos.
    - Introduce global styles, including a CSS reset and custom styles for navigation and movie components.
    - Remove unused globals.css and refactor layout to include navigation and updated metadata.
    - Update styles for home, movie-info, movie-videos, and navigation to enhance UI/UX.
    
    This commit establishes the foundational structure of the movie application, including the main pages and components necessary for displaying movie information. It also sets up styling for a cohesive look and feel across the application.

---
+++ components/navigation.tsx
@@ -0,0 +1,23 @@
+"use client";
+
+import styles from "@/styles/navigation.module.css";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+export default function Navigation() {
+  const path = usePathname();
+  return (
+    <nav className={styles.nav}>
+      <ul>
+        <li>
+          <Link href="/">Home {path === "/" ? "🔥" : ""}</Link>
+        </li>
+        <li>
+          <Link href="about-us">
+            About Us {path === "/about-us" ? "🔥" : ""}
+          </Link>
+        </li>
+      </ul>
+    </nav>
+  );
+}
